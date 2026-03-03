e0e1b0c9c2270dd8f97c1693fd126e3e0e89b9da
rayaanisse014-jpg <rayaanisse014@gmail.com>
2026-03-03 21:18:59 +1000

    Add files via upload

---
+++ T2Q2.py
@@ -0,0 +1,8 @@
+first_word = input('Enter the first word: ')
+second_word = input('Enter the second word: ')
+third_word = input('Enter the third word: ')
+print(f'You entered: {first_word}, {second_word}, {third_word}')
+if first_word == second_word == third_word:
+   print('All three words are the same.')
+else:
+    print('The words are not all the same.')
