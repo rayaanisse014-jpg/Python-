60e6e29e57c879c3ebbacaaadf8c9da1214ca2b7
rayaanisse014-jpg <rayaanisse014@gmail.com>
2026-03-05 11:29:27 +1000

    Add files via upload

--- T2Q10.py
+++ T2Q10.py
@@ -1,11 +1,12 @@
 number = int(input('Enter a positive integer: '))
 original_number = number
-added_digit = 0
+running_total = 0
 while number > 0:
     # Get the last digit
     last_digit = number % 10
      #Add it to the running total
-    added_digit += last_digit 
+     # running_total + running_total + last_digit 
+    running_total += last_digit 
     # Remove the last digit
     number = number // 10
-print(f'The sum of the digits in {original_number} is {added_digit}.') 
+print(f'The sum of the digits in {original_number} is {running_total}.') 
